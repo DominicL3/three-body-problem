1a3a3fc9c503c9c8181dd8a986b890bf52586713
DominicL3 <leducd98@gmail.com>
2019-12-12 00:23:42 -0800

    debugging force calculations

---
+++ test_calculations.py
@@ -0,0 +1,41 @@
+import numpy as np
+import three_body
+
+def test_simple_forces():
+    body1 = three_body.Body(1/6.67e-11, np.array([0, 0, 0]), np.array([0, 0, 0]))
+    body2 = three_body.Body(100, np.array([0, 0, 10]), np.array([0, 0, 0]))
+    F12 = body1.calculate_force([body2]) # force on body1 from body2
+    F21 = body2.calculate_force([body1]) # force on body2 from body1
+
+    # check if vector is correct
+    assert (F12 == np.array([0, 0, 1])).all()
+    assert (F21 == np.array([0, 0, -1])).all()
+
+def test_simple_position_update():
+    body1 = three_body.Body(1/6.67e-11, np.array([0, 0, 0]), np.array([0, 0, 0]))
+    body2 = three_body.Body(100, np.array([0, 0, 10]), np.array([0, 0, 0]))
+
+    body2.update_position([body1], 100)
+    assert np.allclose(body2.position, np.array([0, 0, -90]))
+
+def test_simple_timestep_update():
+    body1 = three_body.Body(1/6.67e-11, np.array([0, 0, 0]), np.array([0, 0, 0]))
+    body2 = three_body.Body(100, np.array([0, 0, 10]), np.array([0, 0, 0]))
+
+    three_body.update_timestep([body1, body2], 100)
+    assert np.allclose(body2.position, np.array([0, 0, -90]))
+
+def test_circular_motion_2body():
+    sun = three_body.Body(1/6.67e-11, np.array([0, 0, 0]), np.array([0, 0, 0]))
+    pebble = three_body.Body(1, np.array([10, 0, 0]), np.array([0, 1/10, 0]))
+
+    # let the clock run a bit
+    for i in np.arange(100):
+        three_body.update_timestep([sun, pebble], 1)
+
+    # ensure radius and velocity is still about the same in circular case
+    radius = np.linalg.norm(sun.position - pebble.position)
+    mag_velocity = np.linalg.norm(pebble.velocity)
+
+    assert np.allclose(radius, 10), f"Radius has changed from 10 to {radius}"
+    assert np.allclose(mag_velocity, 1/10), f"Velocity has changed from 1/10 to {mag_velocity}"
---
+++ three_body.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from IPython.display import display, clear_output
+
+# define classes
+class Body:
+    def __init__(self, mass, initial_position, initial_velocity):
+        assert isinstance(initial_position, np.ndarray)
+        assert isinstance(initial_velocity, np.ndarray)
+
+        self.mass = mass
+        self.position = initial_position.astype('float64')
+        self.velocity = initial_velocity.astype('float64')
+
+    def calculate_force(self, other_bodies):
+        """Calculate the gravitational force magnitude and direction
+        on this body from the other bodies."""
+        G = 6.67e-11 # units of m3 kg−1 s−2
+        F_net = 0
+
+        # compute force from each body
+        for body2 in other_bodies:
+            r12 = body2.position - self.position
+            F12 = G*self.mass*body2.mass/np.linalg.norm(r12)**3 * r12
+            F_net += F12
+
+        return F_net
+
+    def update_position(self, other_bodies, dt):
+        F_net = self.calculate_force(other_bodies)
+        a = F_net / self.mass # Newton's 2nd Law
+        self.velocity += a * dt
+        self.position += self.velocity * dt
+
+        return self.position
+
+def update_timestep(bodies, dt):
+    for i, body in enumerate(bodies):
+        other_bodies = np.delete(bodies, i)
+        body.update_position(other_bodies, dt)
+
+if __name__ == "__main__":
+    # define the three bodies and timestep
+    body1 = Body(100, np.array([0, 0, 10]), np.zeros(3))
+    body2 = Body(10, np.array([10, 0, 0]), np.zeros(3))
+    body3 = Body(10, np.array([0, 10, 0]), np.zeros(3))
+    bodies = [body1, body2, body3]
+    timestep = 3.154e5 # seconds
+    plt.ion()
+    fig = plt.figure(figsize=(8, 6))
+    ax = Axes3D(fig)
+
+    while True:
+        update_timestep(bodies, timestep)
+        positions = [body.position for body in bodies]
+        x, y, z = [p[0] for p in positions], [p[1] for p in positions], [p[2] for p in positions]
+
+        ax.cla()
+        ax.scatter(x, y, z)
+        ax.set(xlim=[-500, 500], ylim=[-500, 500], zlim=[-500, 500])
+        display(fig)
+        clear_output(wait = True)
+        plt.pause(0.05)
+
